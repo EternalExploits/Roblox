75651bd7a4fa7b2932b0a55ad2e08cf17799b19a
Eternal <eternalexploits1337@gmail.com>
2024-08-13 06:32:49 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,15 @@
+local ID = game.PlaceId
+
+if ID == 292439477 then
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EternalExploits/Roblox/main/Phantom_Forces/Script.lua"))()
+
+elseif ID == 12196278347 then
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EternalExploits/Roblox/main/Refinery_Caves_2/Script.lua"))()
+
+else
+
+  print("This game is not supported.")
+
+end
